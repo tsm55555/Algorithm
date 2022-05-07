bdf6118c32a2cebd1295cb413ad91b5f97398132
tsm55555 <57450036+tsm55555@users.noreply.github.com>
2022-05-07 22:58:09 +0800

    q1

--- 程式作業/HW 8/queen.py
+++ 程式作業/HW 8/queen.py
@@ -3,13 +3,13 @@ while(1):
     if(positions[0] == positions[1] == positions[2] == positions[3] == 0):
         break
     
-    if(positions[0] == positions[3] and positions[1] == positions[2]):
-        print("0")
     try:
         slope = (positions[2] - positions[0])/(positions[3] - positions[1])
     except ZeroDivisionError:
         pass  
-    if(positions[0] == positions[2] or positions[1] == positions[3] or abs(slope) == 1):
+    if(positions[0] == positions[3] and positions[1] == positions[2]):
+        print("0")
+    elif(positions[0] == positions[2] or positions[1] == positions[3] or abs(slope) == 1):
         print("1")
     else:
         print("2")
