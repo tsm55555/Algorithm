04790a87aa25b8325e0b2310668017b4fb388e60
tsm55555 <57450036+tsm55555@users.noreply.github.com>
2022-05-23 23:38:45 +0800

    update q3

--- 程式作業/HW 10/q3.py
+++ 程式作業/HW 10/q3.py
@@ -10,11 +10,11 @@ def print_tree(root):
     traversed.append(root)
     while traversed != []:
         ans.append(traversed[0].data)
-        x = traversed.pop(0)
-        if x.left:
-            traversed.append(x.left)
-        if x.right:
-            traversed.append(x.right)
+        tmp = traversed.pop(0)
+        if tmp.left:
+            traversed.append(tmp.left)
+        if tmp.right:
+            traversed.append(tmp.right)
     print(*ans)
 
 test_num = int(input())
@@ -39,11 +39,6 @@ for _ in range(test_num):
         # calculate parent's position
         parent_pos = tree_pos[0:len(tree_pos)-1]
 
-        # print("value: ", tree_val)
-        # print("current_pos: ", tree_pos)
-        # print("parent_pos: ", parent_pos)
-        # print()
-
         # give current node value, create it if not existed
         if tree_pos not in nodes:
             nodes[tree_pos] = node(tree_val)
@@ -53,14 +48,7 @@ for _ in range(test_num):
         # check if root node
         if tree_pos == "":
             root = nodes[tree_pos]
-            # print("Root !!")
-            # print("--------------------------------")
             continue
-
-        # print("current pos data: ", nodes[tree_pos].data)
-        # print("current pos left: ", nodes[tree_pos].left)
-        # print("current pos right: ", nodes[tree_pos].right)  
-        # print() 
 
         # build tree
         if parent_pos in nodes:
@@ -83,11 +71,6 @@ for _ in range(test_num):
                 nodes[parent_pos] = node(-1, left=nodes[tree_pos])
             else:
                 nodes[parent_pos] = node(-1, right=nodes[tree_pos]) 
-
-        # print("parent_pos data: ", nodes[parent_pos].data)
-        # print("parent_pos left: ", nodes[parent_pos].left)
-        # print("parent_pos right: ", nodes[parent_pos].right)
-        # print("--------------------------------")
     
     # if there is a node has -1 value, it's not complete
     not_complete_flag = 0
