0a199daf53964eaa4e94c84bed28d1967a5380b6
tsm55555 <57450036+tsm55555@users.noreply.github.com>
2022-06-08 16:54:55 +0800

    finish q2

---
+++ 程式作業/HW 13/q2.py
@@ -0,0 +1,18 @@
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b%a, a)
+
+def Euler(n):
+    count = 1
+    for i in range(2, n):
+        if gcd(n, i) == 1:
+            count += 1
+    return count
+
+test_num = int(input())
+
+for i in range(test_num):
+    n = int(input())
+    ans = Euler(n)
+    print(ans)
