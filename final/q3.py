4cd7b1fd50ebcf523fd8cd7d29d1162e0a8a1592
tsm55555 <57450036+tsm55555@users.noreply.github.com>
2022-06-17 02:39:17 +0800

    finish final q3

--- final/q3.py
+++ final/q3.py
@@ -1,65 +1,75 @@
-# # not done
+class Node():
+    def __init__(self, x, y, num):
+        self.x = x
+        self.y = y
+        self.num = num
+        self.visited = False
+        self.set = 0
+
+class graph():
+    def __init__(self, num_vertices):
+        self.num_vertices = num_vertices
+        self.edges = {}
+
+    def add_edge(self, u, v, weight):
+        if  u not in self.edges:
+            self.edges[u] = {}
+        if v not in self.edges:
+            self.edges[v] = {}
+        self.edges[u][v] = int(weight)    
+        self.edges[v][u] = int(weight)  
+
+test_num = int(input())
+
+for _ in range(test_num):
+    node_list = []
+    n = int(input())
+    g = graph(n)
+    count = 0
+    for _ in range(n):
+        x, y = list(map(int, input().split()))
+        node = Node(x, y, count)
+        count += 1
+        node.set = count
+        node_list.append(node)
+    # print(node_list)
+    len_list = []
+    for i in node_list:
+        for j in node_list:
+            if i != j:
+                length = abs(i.x - j.x) + abs(i.y - j.y)
+                len_list.append([i, j, length])
+                g.add_edge(i, j, length)
+    len_list.sort(key=lambda x: x[2], reverse=True)
+    # for i in len_list:
+    #     print(i)
 
 
-# class Node():
-#     def __init__(self, x, y, num):
-#         self.x = x
-#         self.y = y
-#         self.num = num
-#         self.visited = 0
-        
-# class graph():
-#     def __init__(self, num_vertices):
-#         self.num_vertices = num_vertices
-#         self.edges = {}
+    min_cost = 0
+    while len_list:
+        current = len_list.pop()
+        # print("current[0].set", current[0].set)
+        # print("current[1].set", current[1].set)
+        if current[0].set != current[1].set:
+            min_cost += current[2]
+            for node in node_list:
+                if node != current[1]:
+                    if node.set ==  current[1].set:
+                        node.set = current[0].set
+            current[1].set = current[0].set
 
-#     def add_edge(self, u, v, weight):
-#         if  u not in self.edges:
-#             self.edges[u] = {}
-#         self.edges[u][v] = int(weight)
+    print(min_cost)
 
-# def find_min(len_list):
-#     min = 9999
-#     for i in len_list:
-#         print(i[2])
-#         if min > i[2]:
-#             min = i[2]
-#     len_list.remove(i)
-#     return min
-
-# test_num = int(input())
-# for _ in range(test_num):
-#     node_list = []
-#     n = int(input())
-#     g= graph(n)
-#     count = 0
-#     for _ in range(n):
-#         x, y = list(map(int, input().split()))
-#         node = Node(x, y, count)
-#         count += 1
-#         node_list.append(node)
-#     len_list = []
-#     for i in node_list:
-#         for j in node_list:
-#             length = abs(i.x - j.x) + abs(i.y - j.y)
-#             len_list.append([i.num, j.num, length])
-#             g.add_edge(i.num, j.num, length)
-#     print(len_list)
-#     # for i in g.edges:
-#     #     print(g.edges[i])
-#     T = []
-#     num_edge = 0
-    
-#     while num_edge < n-1:
-#         min = find_min(len_list)
-    
-
-# '''
-# 2
-# 5
-# 0 0
-# 2 2
-# 3 10
-# 5 2
-# 7 0
-# '''
+'''
+2
+5
+0 0
+2 2
+3 10
+5 2
+7 0
+3
+3 12
+-2 5
+-4 1
+'''
